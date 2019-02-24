ee326907dd62034c3de99423fa5e17f39b203076
frekensnork <nikimik@yandex.ru>
2019-02-24 19:59:01 +0300

    1.18beta

---
+++ tasks1/1.18beta.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+#define MAXLEN 100
+
+void trim(char buf[]);
+int skipSpaces();
+
+int main()
+{
+	char buf[MAXLEN];
+	trim(buf);
+	printf("\ntrim:\n%s", buf);
+	return 0;
+}
+
+void trim(char buf[])
+{
+	int i = 0;
+	int c = getchar();
+	while (c != EOF && i < MAXLEN - 2) {
+                if (c == ' ' || c == '\t') {
+			buf[i++] = ' ';
+			c = skipSpaces();
+	        }
+		if (c == '\n') {
+			// Надо обработать также случай, когда первая строка пустая
+			if (i > 0 && buf[i - 1] == '\n') {
+				i--;
+			}
+			if (i > 1 && buf[i - 1] == ' ' && buf[i - 2] == '\n') {
+				i -= 2;
+			}
+		}
+		if (c != EOF) {
+			buf[i++] = c;
+			c = getchar();
+		}
+	}
+	if (i > 0 && buf[i - 1] != '\n') {
+                buf[i++] = '\n';
+	}
+	buf[i] = '\0';
+}
+
+int skipSpaces() {
+	int c;
+	while ((c = getchar()) == ' ' || c == '\t') {
+	}
+	return c;
+}
